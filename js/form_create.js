5ca577383e121c3b7a59cc5f095bc3361d71eeca
Pasante5 desa <p5@rgtdom.local>
2022-07-26 15:34:34 -0500

    ig-2

---
+++ js/form_create.js
@@ -0,0 +1,8 @@
+const sendDataFormCreate = () => {
+    let name = document.getElementById("name").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+
+    console.log(`${name}${password}${email}`);
+};
+
